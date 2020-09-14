8280db06a30fdee3efaea8cdf6bb6c4dd32cb312
Anson Chao (Surface Book) <gelgoog135@gmail.com>
2020-09-14 21:50:16 +0800

    First Commit

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+    public Text textDebug;
+
+    public void PrintDebug(string name)
+    {
+        textDebug.text = name;
+    }
+
+    public void ChangeScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text txtTimer;
+    float time = 0;
+    bool isEnabled = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtTimer.text = "0:00";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isEnabled)
+        {
+            time += Time.deltaTime;
+            string timeStr = TimeSpan.FromSeconds(time).ToString(@"m\:ss");
+            txtTimer.text = timeStr;
+        }
+
+        if(time > 300)
+        {
+            isEnabled = false;
+        }
+    }
+
+    public void ToggleTimer()
+    {
+        isEnabled = !isEnabled;
+    }
+}
